8f6ce05b9cc1dc53ec992cb1fa0a3c8212f0b921
EduArt2625 <eduardoruiz.0731@gmail.com>
2025-11-06 17:29:26 -0500

    Puerto 5000
    actualizacion del puerto

--- App.py
+++ App.py
@@ -15,15 +15,11 @@ from reportlab.lib.pagesizes import A4
 from flask import Flask
 import os
 
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hola, el servidor Flask está funcionando correctamente en Railway"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
+
+
 
 # -------------------- CARGA DEL MODELO --------------------
 MODEL_PATH = os.path.join(os.getcwd(), "modelo", "modelo_final_inceptionv3.keras")
@@ -50,9 +46,10 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(RESULT_FOLDER, exist_ok=True)
 
 # -------------------- RUTAS DE PÁGINAS --------------------
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return "Hola, el servidor Flask está funcionando correctamente en Railway"
 
 @app.route('/escaneo')
 def escaneo():
@@ -178,4 +175,5 @@ def generar_pdf():
 
 # -------------------- MAIN --------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
